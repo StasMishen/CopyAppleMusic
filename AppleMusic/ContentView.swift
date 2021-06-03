50f7e907a717c59ad49b9a18835378117685949d
Stas <stasmishenkin@mail.ru>
2021-06-03 14:07:32 +0500

    Initial Commit

---
+++ AppleMusic/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppleMusic
+//
+//  Created by Стас on 03.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
